5e7fe5f75cb77b35c6e94165c0d6dc4ebacdfe02
Ingo Wiarda <ingo_wiarda@dewarim.de>
2015-02-16 12:53:24 +0100

    Add example of switch statement with strings.

--- src/de/dewarim/java7/ProjectCoin.java
+++ src/de/dewarim/java7/ProjectCoin.java
@@ -11,12 +11,33 @@ public class ProjectCoin {
          * Syntactic sugar
          */
         int twoFiveFive = 0b11111111;
-        say("An easier way to specify binary values: "+twoFiveFive);
+        say("An easier way to specify binary values: " + twoFiveFive);
         int underscored255 = 0b1111_1111;
-        say("You can now use underscores in numbers to make them easier to read: "+underscored255);
+        say("You can now use underscores in numbers to make them easier to read: " + underscored255);
+
+        say("Be careful, agents of the " + switchWithStrings("AZA") + " are listening");
     }
 
-    public static void say(Object o){
+    public static void say(Object o) {
         System.out.println(String.valueOf(o));
     }
+
+    public static String switchWithStrings(String threeLetterAgency) {
+        String tla;
+        switch (threeLetterAgency) {
+            case "BND":
+                tla = "Bundesnachrichtendienst";
+                break;
+            case "NSA":
+                tla = "National Security Agency";
+                break;
+            case "AZA":
+                tla = "Ananas-Züchter-Alaskas";
+                break;
+            default:
+                tla = "Men in Black";
+        }
+        return tla;
+    }
+
 }
